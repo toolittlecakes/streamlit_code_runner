cb947619dfb50d8fef4b71dff1f95d0604d59da7
sne <nickolay.sheyko@gmail.com>
2024-08-18 13:52:57 +0700

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from code_editor import code_editor
+
+with st.sidebar:
+    run_on_change = st.toggle("Run code on change", value=True)
+    response_dict = code_editor(
+        "st.write('Hello, world!')",
+        height=[20, 20],
+        key="code",
+        lang="python",
+        options={
+            "wrap": True,
+            "showLineNumbers": True,
+            # "displayIndentGuides": True,
+            # "foldStyle": "markbegin",
+            # "foldStyle": "markbeginend",
+        },
+        props={
+            "tabSize": 2,
+            # "focus": True,
+            # "showPrintMargin": True,
+            # "showGutter": False,
+            # "readOnly": True,
+            # "enableBasicAutocompletion": True,
+        },
+        response_mode="debounce",
+        # response_mode=["debounce", "blur"],
+    )
+
+if (
+    response_dict["type"] == "change"
+    and run_on_change
+    or response_dict["type"] == "submit"
+):
+    exec(response_dict["text"])
